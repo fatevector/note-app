d4bb6f87cdfde5aad839273813042d4985ef2dab
Иван Петров <fatevector@yandex.ru>
2023-02-17 17:13:56 +0300

    add POST request processing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require("http");
 const chalk = require("chalk");
 const fs = require("fs/promises");
 const path = require("path");
+const { addNote } = require("./notes.controller");
 
 const port = 3000;
 
@@ -15,6 +16,21 @@ const server = http.createServer(async (req, res) => {
             "Content-Type": "text/html"
         });
         res.end(content);
+    } else if (req.method === "POST") {
+        const body = [];
+        res.writeHead(200, {
+            "Content-Type": "text/plain; charset=utf-8"
+        });
+
+        req.on("data", data => {
+            body.push(data);
+        });
+
+        req.on("end", () => {
+            const title = body.toString().split("=")[1].replaceAll("+", " ");
+            addNote(title);
+            res.end(`Title = ${title}`);
+        });
     }
 });
 
